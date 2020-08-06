8a9cf4f0b769c1a9b2c0ea07fb09c0c940a02b89
Liam Vonberg <62673602+wvonberg@users.noreply.github.com>
2020-08-06 15:06:06 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots';
-
 
 ReactDOM.render(<App />,document.getElementById('root'));
 serviceWorker.unregister();
